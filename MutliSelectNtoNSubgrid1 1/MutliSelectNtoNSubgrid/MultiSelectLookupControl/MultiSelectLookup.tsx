295efb82a0a434981bab25be17bbc70afaf97be3
Bruno Pallinb <pallinb@hotmail.com>
2024-12-05 04:31:24 -0300

    first

---
+++ MutliSelectNtoNSubgrid1 1/MutliSelectNtoNSubgrid/MultiSelectLookupControl/MultiSelectLookup.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+const MultiSelectLookup = (props: any) => {
+    const [formMapptedlookup, setformMapptedlookup] = React.useState(props.lookups);
+    React.useEffect(() => {
+        setformMapptedlookup(props.lookups);
+    }, [props])
+    const OpenLookup = () => {
+        props.OpenLookupObject();
+    }
+    const removeItem = (selectedIt: any) => {
+        setformMapptedlookup(formMapptedlookup.filter((it: any) => !(it.id == selectedIt.id && it.entityType == selectedIt.entityType)));
+        props.RemoveItemFromGrid(selectedIt.id);
+    }
+    return <>
+        <div className='multiSelectLookup'>
+            <div className='lookupObjectListing'>
+                {formMapptedlookup.map((it: any) => {
+                    return <div className='lookupObjectListing-item' key={it.id}>
+                        {props.image && <img src={props.image} alt="" />}
+                        <a href={it.url}>{it.name}</a>
+                        <span className='deleticon' onClick={() => removeItem(it)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" /></svg>
+                        </span>
+
+                    </div>
+                })
+                }
+            </div>
+            <span className='divSearch symbolFont SearchButton-symbol ' onClick={OpenLookup}>
+            </span>
+        </div>
+    </>
+}
+export default React.memo(MultiSelectLookup);
---
+++ MutliSelectNtoNSubgrid1 1/MutliSelectNtoNSubgrid/MultiSelectLookupControl/index.ts
@@ -0,0 +1,205 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+// eslint-disable-next-line no-undef
+import DataSetInterfaces = ComponentFramework.PropertyHelper.DataSetApi;
+type DataSet = ComponentFramework.PropertyTypes.DataSet;
+
+import * as ReactDOM from 'react-dom';
+import { Render } from "./Render";
+export class MultiSelectLookupControl implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    notifyOutputChanged: () => void;
+    container: HTMLDivElement;
+    context: ComponentFramework.Context<IInputs>;
+
+    /**
+     * Empty constructor.
+     */
+    constructor() {
+
+    }
+
+    /**
+     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+     * Data-set values are not initialized here, use updateView.
+     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+     */
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement): void {
+        // Add control initialization code
+
+        this.notifyOutputChanged = notifyOutputChanged;
+        this.container = container;
+        this.context = context;
+        console.log('MultiSelectLookup', this.context)
+
+
+    }
+    RemoveItemFromGrid = (id: any) => {
+        let relationShipName = ((this.context.utils) as any)?._customControlProperties?.descriptor?.Parameters?.["RelationshipName"];
+        let entityToOpen = ((this.context.utils) as any)?._customControlProperties?.descriptor?.Parameters?.["TargetEntityType"];
+        if (!entityToOpen) {
+            entityToOpen = this.context.parameters.records.getTargetEntityType();
+
+        }
+        let primaryEntity = (this.context as any)?.page?.entityTypeName;
+        let primaryEntityId = ((this.context) as any)?.page?.entityId;
+        var disassociateRequest = {
+            getMetadata: () => ({
+                boundParameter: null,
+                parameterTypes: {},
+                operationType: 2,
+                operationName: "Disassociate"
+            }),
+
+
+            relationship: relationShipName,
+
+
+            target: {
+                entityType: primaryEntity,
+                id: primaryEntityId
+            },
+
+            relatedEntityId: id
+        }
+
+        let excuteRef = ((this.context.webAPI) as any).execute;
+        excuteRef(disassociateRequest).then(
+            (success: any) => {
+                console.log("Success", success);
+                this.context.parameters.records.refresh();
+            },
+            (error: any) => {
+                console.log("Error", error);
+            }
+        )
+    }
+    OpenLookups = () => {
+        let relationShipName = ((this.context.utils) as any)?._customControlProperties?.descriptor?.Parameters?.["RelationshipName"];
+        let entityToOpen = ((this.context.utils) as any)?._customControlProperties?.descriptor?.Parameters?.["TargetEntityType"];
+        if (!entityToOpen) {
+            entityToOpen = this.context.parameters.records.getTargetEntityType();
+        }
+        let filterXml = `<filter type="and"></filter>`;
+        let attribute = this.context.parameters.conditionAttribute.raw;
+        let fieldLogicalName = this.context.parameters.filterByField.raw;
+        let allowEmptyFilter = this.context.parameters.allowEmptyFilter.raw === "True";
+        let value = null;
+        if (fieldLogicalName) {
+            switch (Xrm.Page.getAttribute(fieldLogicalName)?.getAttributeType()) {
+                case "lookup":
+                    value = Xrm.Page.getAttribute(fieldLogicalName)?.getValue()?.[0].id;
+                    break;
+                default:
+                    value = Xrm.Page.getAttribute(fieldLogicalName)?.getValue();
+                    break;
+            }
+        }
+        if (attribute) {
+            if (value) {
+                filterXml = `<filter type='and'><condition attribute='${attribute}' operator='eq' value='${value}'/></filter>`;
+            } else if (!allowEmptyFilter) {
+                filterXml = `<filter type='and'><condition attribute='${attribute}' operator='eq' value='00000000-0000-0000-0000-000000000000'/></filter>`;
+            }
+        }
+        let primaryEntity = ((this.context) as any)?.page?.entityTypeName;
+        let primaryEntityId = ((this.context) as any)?.page?.entityId;
+        var lookupOptions: any =
+        {
+            defaultEntityType: entityToOpen,
+            entityTypes: [entityToOpen],
+            allowMultiSelect: true,
+            disableMru: true,
+            filters: [{
+                entityLogicalName: entityToOpen,
+                filterXml: filterXml
+            }]
+
+        };
+        var manyToManyAssociateRequest = {
+            getMetadata: () => ({
+                boundParameter: null,
+                parameterTypes: {},
+                operationType: 2,
+                operationName: "Associate"
+            }),
+            relationship: relationShipName,
+
+            target: {
+                entityType: primaryEntity,
+                id: primaryEntityId
+            },
+        }
+        let excuteRef = ((this.context.webAPI) as any).execute;
+        this.context.utils.lookupObjects(lookupOptions).then((res) => {
+            console.log(res);
+            if (res && res.length > 0) {
+                (manyToManyAssociateRequest as any).relatedEntities =
+                    res.map(it => { return { id: it.id.replace('{', '').replace('}', ''), entityType: it.entityType } });
+                if (excuteRef) {
+                    excuteRef(manyToManyAssociateRequest).then(
+                        (success: any) => {
+                            console.log("Success", success);
+                            this.context.parameters.records.refresh();
+                        },
+                        (error: any) => {
+                            console.log("Error", error);
+                        }
+                    );
+                }
+            }
+        }, err => {
+            console.log(err)
+        });
+    }
+    /**
+     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+     */
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
+        let gridParams = context.parameters.records;
+        let recordOfArray = [];
+        let records = gridParams.records;
+
+        for (let key in records) {
+            let type = "";
+            if ((records as any)[key].getNamedReference)
+                type = (records as any)[key]?.getNamedReference()?.logicalName
+            var record = (records as any)[key];
+            recordOfArray.push({
+                "id": key,
+                url: `?pagetype=entityrecord&etn=${type}&id=${key}`,
+                entityType: type,
+                name: (record._record.fields[record._primaryFieldName]?.value) || "(Nenhum Nome)"
+            })
+        }
+
+
+        let customControlProperties = (this.context?.utils as any)?._customControlProperties;
+        let labelEnabled = customControlProperties?.descriptor?.ShowLabel;
+        let labelText = customControlProperties?.descriptor?.Label;
+        let image = context.parameters.controlImageURl.raw ? context.parameters.controlImageURl.raw : "";
+        Render(recordOfArray, labelEnabled, labelText, image, this.OpenLookups, this.RemoveItemFromGrid, this.container);
+    }
+
+
+    /**
+     * It is called by the framework prior to a control receiving new data.
+     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+     */
+    public getOutputs(): IOutputs {
+        return {
+
+        } as IOutputs;
+    }
+
+    /**
+     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+     * i.e. cancelling any pending remote calls, removing listeners, etc.
+     */
+    public destroy(): void {
+        ReactDOM.unmountComponentAtNode(this.container);
+    }
+
+}
